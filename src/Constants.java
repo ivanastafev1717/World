e9c0850633cd1c481bd1e746e29558553ddf6da5
ivanastafev1717 <ivanastafev1717@gmail.com>
2022-07-07 14:46:28 +0700

    almost done world 2

--- src/Constants.java
+++ src/Constants.java
@@ -1,4 +1,4 @@
 public class Constants {
-    final static int X_AXIS_LENGTH = 2;
-    final static int Y_AXIS_LENGTH = 2;
+    final static int X_AXIS_LENGTH = 100;
+    final static int Y_AXIS_LENGTH = 20;
 }
--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,6 @@ public class Main {
 
         world = generateWorld();
 
-
         // создаем list Creature и заполняем им наш мир
 
         List<CreatureTypes> listOfCreatures = List.of(CreatureTypes.BEAR, CreatureTypes.WOLF, CreatureTypes.SNAKE);
@@ -26,7 +25,7 @@ public class Main {
             generatorCreature.generateCreature(type);
         }
         System.out.println("Создали мир");
-        System.out.println(world);
+        printWorld();
 
         // создаем мапу вероятности съедания
 
@@ -38,49 +37,70 @@ public class Main {
 
         // новый мир после поедания
         System.out.println("Мир после поедания");
-        System.out.println(world);
+        printWorld();
 
         // размножились
 
-       reproductionCreatures(world);
+        reproductionCreatures(world);
 
-       // после размножения
+        // после размножения
         System.out.println("Мир после размножения");
-        System.out.println(world);
+        printWorld();
 
         // создаем процесс движения
         moveWorld();
         System.out.println("Мир после перемещения");
-        System.out.println(world);
-
+        printWorld();
 
     }
+
+    public static void printWorld() {
+        Map<Creature, Integer> log = new LinkedHashMap<>();
+        for (int i = 0; i < Constants.X_AXIS_LENGTH; i++) {
+            for (int j = 0; j < Constants.Y_AXIS_LENGTH; j++) {
+                for (int k = 0; k < world.get(i).get(j).size(); k++) {
+                    Creature creature = world.get(i).get(j).get(k);
+                    if (!log.containsKey(creature)) {
+                        log.put(creature,1);
+                    }
+                    else {
+                        log.put(creature,log.get(creature)+1);
+                    }
+                }
+            }
+        }
+
+        for (Creature c: log.keySet()) {
+            System.out.println(c.getClass().getName() +" " +  log.get(c));
+        }
+
+    }
+
 
     private static void reproductionCreatures(ArrayList<ArrayList<ArrayList<Creature>>> world) {
         for (int i = 0; i < Constants.X_AXIS_LENGTH; i++) {
             for (int j = 0; j < Constants.Y_AXIS_LENGTH; j++) {
                 ArrayList<Creature> creaturesCheck = world.get(i).get(j);
                 creaturesCheck.addAll(getCounts(creaturesCheck));
-                }
+            }
         }
     }
 
     private static List<Creature> getCounts(List<Creature> creatures) {
         Map<Creature, Integer> counts = new HashMap<>();
-        for (Creature creature: creatures) {
+        for (Creature creature : creatures) {
             if (!counts.containsKey(creature)) {
-                counts.put(creature,1);
-            }
-            else {
+                counts.put(creature, 1);
+            } else {
                 int value = counts.get(creature);
-                counts.put(creature,value+1);
+                counts.put(creature, value + 1);
             }
         }
         List<Creature> result = new ArrayList<>();
-        for (Creature creature: counts.keySet()) {
+        for (Creature creature : counts.keySet()) {
             Integer getCreature = counts.get(creature);
-            if (getCreature >=2) {
-                int valueNewCreature = getCreature/2;
+            if (getCreature >= 2) {
+                int valueNewCreature = getCreature / 2;
                 for (int i = 0; i < valueNewCreature; i++) {
                     result.add(creature);
                 }
@@ -91,62 +111,68 @@ public class Main {
 
 
     private static void moveWorld() {
-        int move;
-        ArrayList<String> canMove = new ArrayList<>();
+        int Steps;
+
+        ArrayList<ArrayList<ArrayList<Creature>>> tempWorld = (ArrayList<ArrayList<ArrayList<Creature>>>) world.clone();
+
         for (int i = 0; i < world.size(); i++) {
             for (int j = 0; j < world.get(i).size(); j++) {
                 for (int k = 0; k < world.get(i).get(j).size(); k++) {
-                    //количество шагов от 0 до max
-                    move = (int) (Math.round(Math.random() * (world.get(i).get(j).get(k).speed+1)));
-                    //пофиксить что по x и y разное количество шагов
-                    if (move!=0 && move<Constants.X_AXIS_LENGTH && move<Constants.Y_AXIS_LENGTH) {
-                        if (i - move >= 0) {
-                            canMove.add("left");
-                        }
-                        if (i + move < Constants.X_AXIS_LENGTH ) {
-                            canMove.add("right");
-                        }
-                        if (j - move >= 0) {
-                            canMove.add("up");
-                        }
-                        if (j + move < Constants.Y_AXIS_LENGTH) {
-                            canMove.add("down");
-                        }
-                    int makeMove = (int) (Math.random() * canMove.size());
-                    String getWay = canMove.get(makeMove);
+                    ArrayList<String> canMove = new ArrayList<>();
+                    Steps = (int) (Math.round(Math.random() * (world.get(i).get(j).get(k).speed + 1)));
+                    if (Steps != 0 && Steps < Constants.X_AXIS_LENGTH - Steps && Steps < Constants.Y_AXIS_LENGTH - Steps) {
+                        addWays(Steps, canMove, i, j);
+                        int realSteps = (int) (Math.random() * canMove.size());
+                        String getWay = canMove.get(realSteps);
                         Creature temp = getCreature(i, j, k);
-                      // правильно ли раставлены оси?
+
                         switch (getWay) {
                             case "left" -> {
-                                System.out.println(world.get(i).get(j).get(k) + " [" + i + j + k + "] двигается " + getWay +" на " + move);
-                                world.get(i-move).get(j).add(temp);
+                                tempWorld.get(i - Steps).get(j).add(temp);
+                                removeCreature(tempWorld, i, j, k);
                             }
                             case "right" -> {
-                                System.out.println(world.get(i).get(j).get(k) + " [" + i + j + k + "] двигается " + getWay +" на " + move);
-                                world.get(i+move).get(j).add(temp);
-                        }
+                                tempWorld.get(i + Steps).get(j).add(temp);
+                                removeCreature(tempWorld, i, j, k);
+                            }
                             case "up" -> {
-                                System.out.println(world.get(i).get(j).get(k) + " [" + i + j + k + "] двигается "  + getWay +" на " + move);
-                                world.get(i).get(j-move).add(temp);
+                                tempWorld.get(i).get(j - Steps).add(temp);
+                                removeCreature(tempWorld, i, j, k);
                             }
 
-                            default -> {
-                                System.out.println(world.get(i).get(j).get(k) + " [" + i + j + k + "] двигается " +getWay + " на " + makeMove +"");
-                                world.get(i).get(j+move).add(temp);
+                            case "down" -> {
+                                tempWorld.get(i).get(j + Steps).add(temp);
+                                removeCreature(tempWorld, i, j, k);
                             }
                         }
                     }
-                    System.out.println(world.get(i).get(j).get(k) + " [" + i + j + k + "] стоит на месте");
-                    canMove = new ArrayList<>();
                 }
             }
+        }
+        world = tempWorld;
+    }
+
+    private static void removeCreature(ArrayList<ArrayList<ArrayList<Creature>>> tempWorld, int i, int j, int k) {
+        tempWorld.get(i).get(j).remove(world.get(i).get(j).get(k));
+    }
+
+    private static void addWays(int move, ArrayList<String> canMove, int i, int j) {
+        if (i - move >= 0) {
+            canMove.add("left");
+        }
+        if (i + move < Constants.X_AXIS_LENGTH) {
+            canMove.add("right");
+        }
+        if (j - move >= 0) {
+            canMove.add("up");
+        }
+        if (j + move < Constants.Y_AXIS_LENGTH) {
+            canMove.add("down");
         }
     }
 
     private static Creature getCreature(int i, int j, int k) {
-        Creature temp = world.get(i).get(j).get(k);
-        world.get(i).get(j).remove(k);
-        return temp;
+        return world.get(i).get(j).get(k);
     }
 
     private static void CreaturesEating(Map<CreatureTypes, Map<CreatureTypes, Integer>> mapEatableProbability) throws InterruptedException {
@@ -160,38 +186,26 @@ public class Main {
                 for (int k = 0; k < world.get(i).get(j).size(); k++) {
                     attackerCreature = world.get(i).get(j).get(attackerCreaturePosition);
                     defenderCreature = world.get(i).get(j).get(k);
-                    while (world.get(i).get(j).get(attackerCreaturePosition).getClass() != world.get(i).get(j).get(k).getClass())
-                            {
+                    while (world.get(i).get(j).get(attackerCreaturePosition).getClass() != world.get(i).get(j).get(k).getClass()) {
                         defenderCreaturePosition = k;
-//                      System.out.println("Attacker " + attackerCreature + " " + attackerCreaturePosition);
-//                      System.out.println("Defender " + defenderCreature + " " + defenderCreaturePosition);
+
                         int eatablePercents = mapEatableProbability.get(CreatureTypes.valueOf(attackerCreature.toString().toUpperCase())).get(CreatureTypes.valueOf(defenderCreature.toString().toUpperCase()));
                         int fightPercent = (int) (Math.round(Math.random() * 100));
 
                         if (eatablePercents > fightPercent) {
 
-//                            System.out.println("Выпало "+fightPercent+"<"+eatablePercents+" - " + attackerCreature + " съедает " + defenderCreature);
                             world.get(i).get(j).remove(defenderCreaturePosition);
-                 //   System.out.print("Удалили - " + defenderCreature + "["+i+","+j+","+defenderCreaturePosition+"]"+"    ");
-                     //       System.out.println("defenderCreaturePosition="+defenderCreaturePosition+" world.get(i).get(j).size()-1)+"+(world.get(i).get(j).size()-1));
-                            if (defenderCreaturePosition==world.get(i).get(j).size()-1){
-                           break;
+                            if (defenderCreaturePosition == world.get(i).get(j).size() - 1) {
+                                break;
                             }
-
-                        } else {
-//                            System.out.println("Выпало "+fightPercent+">"+eatablePercents+" - " +attackerCreature + " проходит мимо " + defenderCreature);
+                            if (world.get(i).get(j).size() - 1 < attackerCreaturePosition) attackerCreaturePosition++;
+                            else break;
                         }
-                           if (world.get(i).get(j).size()-1<attackerCreaturePosition) attackerCreaturePosition++;
-                           else break;
-//                                System.out.println();
                     }
-
+                    attackerCreaturePosition = 0;
                 }
-//                System.out.println("-------------------------------");
-                attackerCreaturePosition=0;
             }
         }
-
     }
 
 
@@ -209,7 +223,6 @@ public class Main {
 
         return mapEatableProbability;
     }
-
 
 
     private static ArrayList<ArrayList<ArrayList<Creature>>> generateWorld() {
